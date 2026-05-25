5134563d13757d543ae6189a407ce66086fcef86
AldinCausi <aldincausi33@gmail.com>
2026-05-25 15:43:02 +0200

    add_appliactions erweitert um Funktionalität mit data.json, Daten werden nun außerhalb gespeichert

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Das hier ist eine Job application tracker software. Start 24.05.2026 17:23
-
+import json
 
 # Menu darstellen
 def menu():
@@ -12,61 +12,45 @@ def menu():
             break
         if int(userInput) == 1:
             add_application()
+        if int(userInput) == 2:
+            show_applications()
 
 
 
 #Bewerbung hinzufügen
 def add_application():
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+    except json.JSONDecodeError as e: 
+        data = {}
 
-    dict_applications = {}
+    name = input("Gebe den Namen der Firma ein\n> ")
+    job = input("Gebe die Jobbezeichnung ein\n> ")
 
-    status_default = "Wartet auf Rückmeldung"
-
-    print("Gebe den Namen der Firma\n")
-    comp_name = input("> ")
-
-    print("Gebe die Jobbezeichnung ein\n")
-    job_name = input("> ")
-
-    dict_applications.update(
-        {
-            comp_name: {
-            "Job": job_name, 
-            "Status": status_default
-            }
-        }
-    )
-
-    with open("Bewerbungen.txt", "w") as f:
-        for key, info in dict_applications.items():
-            f.write(f"{key}\n")
-            f.write(f"\t\tStatus: {info["Status"]}\n")
-            f.write(f"\t\tJob: {info["Job"]}\n")
-
-# Hier werden Dinge an Beispielen ausprobiert
-def test():
-    test_dict = {
-            "Bewerbung_1": {
-            "Firma": "Thyssenkrupp",
-            "Job": "Softwarewntwickler",
-            "Status": "Wartet auf Rückmeldung"
-        }
+    data[name] = {
+        "Status": "Wartet",
+        "Job": job
     }
 
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
 
-    with open("test.txt", "a") as f:
-        for key, info in test_dict.items():
-            f.write(f"{key}: ")
-            f.write(f"{info["Status"]}\n")
-            f.write(f"\tJob: {info["Job"]}\n")
-            f.write(f"\tFirma: {info["Firma"]}")
+    with open("data.json", "r") as f:
+        data = json.load(f)
 
-print("1. Test\n")
-print("2. Tracker\n")
-input_test = input("> ")
-if input_test == "1":
-    test()
-if input_test == "2":
-    menu()
+    with open("Bewerbungen.txt", "w") as f:
+        for key, info in data.items():
+            f.write(f"{key}\n")
+            f.write(f"\t\tSTATUS: {info["Status"]}\n")
+            f.write(f"\t\tJob: {info["Job"]}\n")
+
+# Bewerbungen ansehen
+def show_applications():
+    with open("Bewerbungen.txt", "r") as f:
+        content = f.read()
+    print(content)
 
 
+if __name__ == "__main__":
+    menU()
