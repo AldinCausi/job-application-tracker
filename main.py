62f09e0bf922a083b2a582547bbf4466e3c4e7f8
AldinCausi <aldincausi33@gmail.com>
2026-06-05 01:27:32 +0200

    Wechsel zu Datenbank mit SQLite, add, delete, update hinzugefügt

--- main.py
+++ main.py
@@ -1,55 +1,132 @@
-# Das hier ist eine Job application tracker software. Start 24.05.2026 17:23
-import json
+import sqlite3
+import os
 
-# Menu darstellen
-def menu():
+def clear_screen():
+    os.system("clear")
+
+
+conn  = sqlite3.connect("bewerbungen.db")
+cursor = conn.cursor()
+
+def create_table():
+    cursor.execute("""CREATE TABLE IF NOT EXISTS bewerbungen (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   company TEXT,
+                   role TEXT,
+                   status TEXT)
+                """)
+
+def add_application(company, role, status):
+    cursor.execute(
+        "INSERT INTO bewerbungen(company, role, status) VALUES (?,?,?)", (company, role, status)
+        )
+    conn.commit()
+
+
+def show_applications():
+    clear_screen()
+    cursor.execute("SELECT * FROM bewerbungen")
+    result = cursor.fetchall()
+
+    if result == []:
+        print("\nKeine Bewerbungen\n")
+    
+    for row in result:
+        print("-" * 20)
+        print(f"ID: {row[0]}")
+        print(f"Company: {row[1]}")
+        print(f"Role: {row[2]}")
+        print(f"Status: {row[3]}")
+
+def delete_byID(id):
+    cursor.execute("DELETE FROM bewerbungen WHERE id = ?", (id,))
+    conn.commit()
+
+def delete_by_company(company):
+    cursor.execute("DELETE FROM bewerbungen WHERE company = ?", (company, ))
+    conn.commit()
+
+def delete_by_role(role):
+    cursor.execute("DELETE FROM bewerbungen WHERE role = ?", (role, ))
+    conn.commit()
+
+def update_status(status, id):
+    cursor.execute("UPDATE bewerbungen SET status = ? WHERE id = ?", (status, id))
+    conn.commit()
+
+if __name__ == "__main__":
+    create_table()
+    default_status = "pending"
+
+
     while True:
-        print("1. Bewerbung hinzufügen\n")
-        print("2. Bewerbungen ansehen\n")
-        print("Gebe 'q' ein, um das Programm zu beenden.\n")
-        userInput = input("> ")
-        if userInput == 'q':
-            break
-        if int(userInput) == 1:
-            add_application()
-        if int(userInput) == 2:
+        print("\n1. Bewerbung hinzufügen")
+        print("2. Bewerbungen anzeigen")
+        print("3. Status einer Bewerbung ändern")
+        print("4. Bewerbung löschen")
+        print("5. Beenden")
+
+        choice = input("> ")
+
+        if choice == "1":
+            clear_screen()
+            company = input("> Company: ")
+            role = input("> Role: ")
+            status = default_status
+            add_application(company, role, status)
+
+        elif choice == "2":
+
             show_applications()
 
+        elif choice == "3":
 
+            status_id = int(input("> ID: "))
+            new_status = input("> Neuer Status: ")
+            update_status(new_status, status_id)
 
-#Bewerbung hinzufügen
-def add_application():
-    try:
-        with open("data.json", "r") as f:
-            data = json.load(f)
-    except json.JSONDecodeError as e: 
-        data = {}
+        elif choice == "4":
 
-    name = input("Gebe den Namen der Firma ein\n> ")
-    job = input("Gebe die Jobbezeichnung ein\n> ")
+            while True:
+                
+                print("\n1. Lösche mit spezifischer ID")
+                print("2. Lösche alle einer Company")
+                print("3. Lösche alle eines Jobs")
+                print("4. Zurück")
 
-    data[name] = {
-        "Status": "Wartet",
-        "Job": job
-    }
+                del_choice = input("> ")
 
-    with open("data.json", "w") as f:
-        json.dump(data, f, indent=4)
+                if del_choice == "1":
+                    
+                    application_id = int(input("> ID: "))
+                    delete_byID(application_id)
+                    break
 
-    with open("data.json", "r") as f:
-        data = json.load(f)
+                elif del_choice == "2":
 
-    with open("Bewerbungen.txt", "w") as f:
-        for key, info in data.items():
-            f.write(f"{key}\n")
-            f.write(f"\t\tSTATUS: {info["Status"]}\n")
-            f.write(f"\t\tJob: {info["Job"]}\n")
+                    company_del = input("> Company: ")
+                    delete_by_company(company_del)
+                    break
 
-# Bewerbungen ansehen
-def show_applications():
-    with open("Bewerbungen.txt", "r") as f:
-        content = f.read()
-    print(content)
+                elif del_choice == "3":
 
+                    role_del = input("> Role: ")
+                    delete_by_role(role_del)
+                    break
 
-menU()
+                elif del_choice == "4":
+                    clear_screen()
+                    break
+                
+                else:
+                    print("Gib eine Zahl von 1-4 ein.")
+                    continue
+
+        elif choice == "5":
+            conn.close()
+            break
+        
+        else:
+            print("Gib eine Zahl von 1-4 ein.")
+            continue
+
