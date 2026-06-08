d6962663d21df7d903ad3f300c42c90f7d3076ee
AldinCausi <aldincausi33@gmail.com>
2026-06-08 19:05:12 +0200

    databse.py auf klassenstruktur gewechsel

--- database.py
+++ database.py
@@ -1,53 +1,68 @@
 import sqlite3
 import utils
 
-conn  = sqlite3.connect("bewerbungen.db")
-cursor = conn.cursor()
+class Database:
+    def __init__(self, db_name="bewerbungen.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.create_table()
 
-def create_table():
-    cursor.execute("""CREATE TABLE IF NOT EXISTS bewerbungen (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   company TEXT,
-                   role TEXT,
-                   status TEXT)
-                """)
-    
-def delete_table():
-    cursor.execute("DROP TABLE bewerbungen")
+    def create_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS bewerbungen (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    company TEXT,
+                    role TEXT,
+                    status TEXT)
+                    """)
 
-def add_application(company, role, status):
-    cursor.execute(
-        "INSERT INTO bewerbungen(company, role, status) VALUES (?,?,?)", (company, role, status)
-        )
-    conn.commit()
+    def add_application(self, company, role, status):
+        self.cursor.execute(
+            "INSERT INTO bewerbungen(company, role, status) VALUES (?,?,?)", (company, role, status)
+            )
+        self.conn.commit()
 
-def show_applications():
-    utils.clear_screen()
-    cursor.execute("SELECT * FROM bewerbungen")
-    result = cursor.fetchall()
+    def show_applications(self):
+        utils.clear_screen()
+        self.cursor.execute("SELECT * FROM bewerbungen")
+        result = self.cursor.fetchall()
 
-    if result == []:
-        print("\nKeine Bewerbungen\n")
-    
-    for row in result:
-        print("-" * 20)
-        print(f"ID: {row[0]}")
-        print(f"Company: {row[1]}")
-        print(f"Role: {row[2]}")
-        print(f"Status: {row[3]}")
+        if result == []:
+            print("\nKeine Bewerbungen\n")
+        
+        for row in result:
+            print("-" * 20)
+            print(f"ID: {row[0]}")
+            print(f"Company: {row[1]}")
+            print(f"Role: {row[2]}")
+            print(f"Status: {row[3]}")
 
-def delete_byID(id):
-    cursor.execute("DELETE FROM bewerbungen WHERE id = ?", (id,))
-    conn.commit()
+    def delete_byID(self, id):
+        self.cursor.execute("DELETE FROM bewerbungen WHERE id = ?", (id,))
+        self.conn.commit()
 
-def delete_by_company(company):
-    cursor.execute("DELETE FROM bewerbungen WHERE company = ?", (company, ))
-    conn.commit()
+    def delete_by_company(self, company):
+        self.cursor.execute("DELETE FROM bewerbungen WHERE company = ?", (company, ))
+        self.conn.commit()
 
-def delete_by_role(role):
-    cursor.execute("DELETE FROM bewerbungen WHERE role = ?", (role, ))
-    conn.commit()
+    def delete_by_role(self, role):
+        self.cursor.execute("DELETE FROM bewerbungen WHERE role = ?", (role, ))
+        self.conn.commit()
 
-def update_status(status, id):
-    cursor.execute("UPDATE bewerbungen SET status = ? WHERE id = ?", (status, id))
-    conn.commit()
+    def update_status(self, status, id):
+        self.cursor.execute("UPDATE bewerbungen SET status = ? WHERE id = ?", (status, id))
+        self.conn.commit()
+
+    def show_with_status(self, status):
+        utils.clear_screen()
+        self.cursor.execute("SELECT * FROM bewerbungen WHERE status = ?", (status, ))
+        result = self.cursor.fetchall()
+
+        if result == []:
+            print(f"Keine Bewerbungen mit status={status}\n")
+        
+        for row in result:
+            print("-" * 20)
+            print(f"ID: {row[0]}")
+            print(f"Company: {row[1]}")
+            print(f"Role: {row[2]}")
+            print(f"Status: {row[3]}")
--- main.py
+++ main.py
@@ -1,47 +1,44 @@
-import database
+from database import Database
 import sys
+
 
 if __name__ == "__main__":
 
-    # Das hier erlaubt es dem Nutzer mit einer leeren database zu starten
-    if len(sys.argv) > 1:
-        argument = sys.argv[1]
-        if argument == "1":
-            database.delete_table()
+    db = Database()
 
+    if len(sys.argv) > 1 and sys.argv[1] == "1":
+        db.cursor.execute("DROP TABLE bewerbungen")
+        db.conn.commit()
+        db.conn.close()
+        sys.exit(0)
 
-    database.create_table()
-    default_status = "pending"
-
+    
+    default_status = "applied"
 
     while True:
         print("\n1. Bewerbung hinzufügen")
         print("2. Bewerbungen anzeigen")
-        print("3. Status einer Bewerbung ändern")
+        print("3. Status ändern")
         print("4. Bewerbung löschen")
-        print("5. Beenden")
+        print("5. Filter nach Status")
+        print("6. Beenden")
 
         choice = input("> ")
 
         if choice == "1":
-            database.create_table()
             company = input("> Company: ")
             role = input("> Role: ")
-            status = default_status
-            database.add_application(company, role, status)
+            db.add_application(company, role, default_status)
 
         elif choice == "2":
-
-            database.show_applications()
+            db.show_applications()
 
         elif choice == "3":
-
-            status_id = int(input("> ID: "))
-            new_status = input("> Neuer Status: ")
-            database.update_status(new_status, status_id)
+            id = int(input("> ID: "))
+            status = input("> Neuer Status: ")
+            db.update_status(status, id)
 
         elif choice == "4":
-
             while True:
                 
                 print("\n1. Lösche mit spezifischer ID")
@@ -54,23 +51,23 @@ if __name__ == "__main__":
                 if del_choice == "1":
                     
                     application_id = int(input("> ID: "))
-                    database.delete_byID(application_id)
+                    db.delete_byID(application_id)
                     break
 
                 elif del_choice == "2":
 
                     company_del = input("> Company: ")
-                    database.delete_by_company(company_del)
+                    db.delete_by_company(company_del)
                     break
 
                 elif del_choice == "3":
 
                     role_del = input("> Role: ")
-                    database.delete_by_role(role_del)
+                    db.delete_by_role(role_del)
                     break
 
                 elif del_choice == "4":
-                    database.create_table()
+                    db.create_table()
                     break
                 
                 else:
@@ -78,9 +75,9 @@ if __name__ == "__main__":
                     continue
 
         elif choice == "5":
-            database.conn.close()
+            status = input("> Status: ")
+            db.show_with_status(status)
+
+        elif choice == "6":
+            db.conn.close()
             break
-        
-        else:
-            print("Gib eine Zahl von 1-4 ein.")
-            continue
